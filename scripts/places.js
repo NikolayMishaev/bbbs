65afcb55a79d6527389238929c6ccc67239c9395
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-05-20 17:34:12 +0300

    верстка формы на странице Куда пойти

--- scripts/places.js
+++ scripts/places.js
@@ -1,6 +1,16 @@
 const popupAdviceForm = document.querySelector('.popup-advice');
 const buttonAdviceForm = document.querySelector('.block-message__button');
+const buttonFormSelect = document.querySelector('.popup__select');
+const popupAdviceContainer = document.querySelector('.popup__container_type_advice');
 
 buttonAdviceForm.addEventListener('click', ()=> {
   openPopup(popupAdviceForm);
+})
+
+buttonFormSelect.addEventListener('click',()=>{
+  buttonFormSelect.classList.toggle('popup__select_type_active');
+})
+
+popupAdviceContainer.addEventListener('click',(e)=>{
+  e.target.classList.contains("popup__select")? "" : buttonFormSelect.classList.remove('popup__select_type_active');
 })
