0377198e435f62838c63e0e82a49409338ca8521
Michael2M <77737524+Michael2M-dot@users.noreply.github.com>
2021-05-25 21:27:23 +0800

    added new sections

--- scripts/questions.js
+++ scripts/questions.js
@@ -1,9 +1,12 @@
-const buttonsToggleAnswer = document.querySelectorAll('.block-questions__button');
-const answersToQuestions = document.querySelectorAll('.block-questions__answer');
+const buttonsToggleAnswer = document.querySelectorAll(
+  ".block-questions__button"
+);
+const answersToQuestions = document.querySelectorAll(
+  ".block-questions__answer"
+);
 
-
-buttonsToggleAnswer.forEach((button,i)=>{
-  button.addEventListener('click',()=>{
-    answersToQuestions[i].classList.toggle('display-block');
-  })
-})
+buttonsToggleAnswer.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    answersToQuestions[i].classList.toggle("display-block");
+  });
+});
