3b106dfa0f52e1d4da90e7b0fb6e3413c3bfb099
Nikolay Mishaev <nikolay.mishaev@yandex.ru>
2021-05-28 22:33:13 +0300

    добавлена анимация на нажатие кнопки с вопросом на странице Вопросы

--- scripts/questions.js
+++ scripts/questions.js
@@ -4,6 +4,7 @@ const buttonsQuestionTitle = document.querySelectorAll('.block-questions__title'
 
 buttonsQuestionTitle.forEach((button,i)=>{
   button.addEventListener('click',()=>{
+    buttonsToggleAnswer[i].classList.toggle('block-questions__button_active');
     answersToQuestions[i].classList.toggle('display-block');
   })
 })
